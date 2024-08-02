4c4f416e3d1cf5acf042999fc87728ccd50c5c15
Gonza <gnzx89@gmail.com>
2024-08-02 11:26:28 -0300

    version celu

--- src/Componentes/Votos.jsx
+++ src/Componentes/Votos.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import '../Estilos/votos.css'
 
 const Votos = (props) => {
+
+    const [votoClickeado , setVotoClickeado] = useState(false);
+
+    useEffect(() => {
+        votoClickeado &&
+        setTimeout(() => {
+            setVotoClickeado(false)
+        }, 100);
+    },[votoClickeado])
 
     const enviarVotos = () => {
         return (props.enviarId(props.nombreCandidato))
     }
 
     return (
-        <div className='boleta-container'>
+        <div className={votoClickeado ? 'boleta-container votoPresionado' : 'boleta-container'}>
             <img 
-            onClick={()=>enviarVotos(props.children)}
+            onClick={()=>enviarVotos(props.children , setVotoClickeado(true))}
             src={props.boleta} alt="" />
         </div>
     )
